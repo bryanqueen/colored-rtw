11e4f0185c5b4b9c9bdc4e3e547d71fa8d8dc364
bryanqueen <somtochukwuumehdaniel@gmail.com>
2026-01-30 00:28:25 +0100

    feat: Remove image unoptimization and add Shopify and Salesforce Commerce Cloud hostnames to Next.js image configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -12,9 +12,12 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+      },
       {
         protocol: 'https',
         hostname: 'zylq-002.dx.commercecloud.salesforce.com',
